c3ff26b4fd3d2e21cb739452b89aa59e208a3ff0
Sébastien Lampazona <sebastien@edusign.fr>
2024-05-24 14:11:31 +0200

    Correction tests

--- tests/signatureValidation.test.ts
+++ tests/signatureValidation.test.ts
@@ -1,4 +1,5 @@
-import base64MockedFile from './mocks/base64File.mock';
+import base64GoodSignature from './mocks/base64GoodSignature.mock';
+import base64BadSignature from './mocks/base64BadSignature.mock';
 import request from "supertest";
 import app from '../src/app';
 
@@ -11,20 +12,20 @@ describe('Test the signature validation route', () => {
 
     it('should return an error if there is bad parameter sended', async () => {
         const response = await request(app).post('/signature/validation')
-            .attach('badParameter', Buffer.from(base64MockedFile, 'base64'));
+            .attach('badParameter', Buffer.from(base64GoodSignature, 'base64'));
         expect(response.statusCode).toBe(400);
     });
 
     it('should return an error if the signature is not validated', async () => {
         const response = await request(app).post('/signature/validation')
-            .attach('signature', Buffer.from(base64MockedFile, 'base64'), 'signature.png')
+            .attach('signature', Buffer.from(base64BadSignature, 'base64'), 'signature.png')
         expect(response.statusCode).toBe(400);
         expect(response.body).toEqual({ message: "Invalid file provided" });
     });
 
     it('should return a success if the signature is validated', async () => {
         const response = await request(app).post('/signature/validation')
-            .attach('signature', Buffer.from(base64MockedFile, 'base64'), 'signature.png')
+            .attach('signature', Buffer.from(base64GoodSignature, 'base64'), 'signature.png')
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({ message: "Valid signature" });
     });
